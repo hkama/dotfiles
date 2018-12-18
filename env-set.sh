e4e57dce30f9305ecec0908b8f66898cce1fe989
Hitoshi Kamada <hitoshi.kamada@tri-ad.global>
2018-12-18 17:08:33 +0900

    fix

--- env-set.sh
+++ env-set.sh
@@ -7,7 +7,7 @@ sudo apt -o install global
 # emacs25に入れ替え
 sudo http_proxy=${HTTP_PROXY} https_proxy=${HTTP_PROXY} add-apt-repository ppa:ubuntu-elisp/ppa
 sudo http_proxy=${HTTP_PROXY} https_proxy=${HTTP_PROXY} apt-get update
-sudo http_proxy=${HTTP_PROXY} https_proxy=${HTTP_PROXY} apt-get install emacs25 cmigemo
+sudo http_proxy=${HTTP_PROXY} https_proxy=${HTTP_PROXY} apt-get install emacs25 cmigemo kazam
 sudo http_proxy=${HTTP_PROXY} https_proxy=${HTTP_PROXY} update-alternatives --config emacs
 
 sudo apt -o Acquire::http::proxy=$HTTP_PROXY install xsel
