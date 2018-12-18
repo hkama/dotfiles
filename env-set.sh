177302a83cc677fa65caf3a135a70c72f081f37f
Hitoshi Kamada <hitoshi.kamada@tri-ad.global>
2018-12-18 16:02:19 +0900

    fix

--- env-set.sh
+++ env-set.sh
@@ -2,4 +2,4 @@
 cp .emacs ~/.emacs
 curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb
 sudo dpkg -i ripgrep_0.10.0_amd64.deb
-sudo apt install global
+sudo apt -o install global
